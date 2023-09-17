89637775a836b1bf58f0e15637bc824a13ca5b21
Rohit <rkumar8092378845@gmail.com>
2023-09-17 22:36:58 +0530

    - fixed build issue
    #1

--- src/test/java/com/rkumar0206/mymexpensecategoryservice/MymExpenseCategoryServiceApplicationTests.java
+++ src/test/java/com/rkumar0206/mymexpensecategoryservice/MymExpenseCategoryServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.rkumar0206.mymexpensecategoryservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration(classes = {MymExpenseCategoryServiceApplication.class}, loader = SpringBootContextLoader.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MymExpenseCategoryServiceApplicationTests {
 
 	@Test
